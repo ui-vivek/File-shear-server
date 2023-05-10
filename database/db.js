5b53ca341a56a40ab5ae012e3cb392b6d4ad0848
Vivek singh <cu.vivek.1557@gmail.com>
2023-05-10 10:17:46 +0530

    test

---
+++ database/db.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const DBConnection = async () => {
+    const USERNAME = process.env.DB_USERNAME;
+    const PASSWORD = process.env.DB_PASSWORD;
+
+    const MONGO_URI = `mongodb+srv://csvfilecheckernodejs-user:csvfilecheckernodejs-user@cluster0.lzeuen4.mongodb.net/Files?retryWrites=true&w=majority`;
+    try {
+        await mongoose.connect(MONGO_URI, { useNewUrlParser: true });
+        console.log('Database connected successfully');
+    } catch (error) {
+        console.log('Error while connecting with the database ', error.message);
+    }
+}
+
+export default DBConnection;
